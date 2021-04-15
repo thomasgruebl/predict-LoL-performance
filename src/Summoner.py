561dc9c36d9f0654bca635dc53d7b63f15eaba6e
thomasgruebl <thomasgruebl@gmx.at>
2021-04-15 22:20:09 +0200

    updated constructor + removed old stuff + added champion v champion performance

--- src/Summoner.py
+++ src/Summoner.py
@@ -63,11 +63,10 @@ class Summoner(object):
             win_loss_per_weekday[k] = [0, 0]
 
         for idx, match in enumerate(self.match_data):
+            participant_id = self._get_participant_id_from_summoner_name(self.participants[idx])
             unixtime = int(match['gameCreation']) / 1000
             weekday = datetime.utcfromtimestamp(unixtime).strftime('%A')
-
             for i in range(0, 10):
-                participant_id = self._get_participant_id_from_summoner_name(self.participants[idx])
                 # print(f"Participant ID of match {idx} is {participantId}")
                 if match['participants'][i]['participantId'] == participant_id:
                     # print(match['participants'][i]['participantId'])
@@ -97,6 +96,30 @@ class Summoner(object):
         plt.show()
 
         return win_loss_per_weekday
+
+    def get_champion_v_champion_performance(self):
+
+        # NOTE: EXCLUDE EVERYTHING EXCEPT 'gameMode': 'CLASSIC'
+
+        # We need a lookup table for championIds -> names
+
+        # HEATMAP Matrix comparison of Champ v Champ performance?
+
+        for idx, match in enumerate(self.match_data):
+            participant_id = self._get_participant_id_from_summoner_name(self.participants[idx])
+            champion_id = match['participants'][participant_id - 1]['championId']
+            print(f"Part ID {participant_id} and champ is {champion_id}")
+            for i in range(0, 10):
+                if match['participants'][i]['timeline']['lane'] == match['participants'][participant_id - 1]['timeline']['lane'] \
+                        and (match['participants'][i]['participantId'] != participant_id):
+                    opponent_id = match['participants'][i]['participantId']
+                    opponent_champion = match['participants'][opponent_id - 1]['championId']
+                    if match['participants'][i]['timeline']['role'] == match['participants'][participant_id - 1]['timeline']['role']:
+                        opponent_id = match['participants'][i]['participantId']
+                        opponent_champion = match['participants'][opponent_id - 1]['championId']
+                    print(f"OPP ID {opponent_id} and champ is {opponent_champion}")
+
+
 
     def predict_weekday_performance(self):
         # get_weekday_performance()
