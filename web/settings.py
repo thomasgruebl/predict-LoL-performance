83451cae9ac964d595e5c20304eb36ab80f5d073
thomasgruebl <thomasgruebl@gmx.at>
2021-07-09 22:13:21 +0200

    updating variables

--- web/settings.py
+++ web/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k94a!c88)x%qrzx!&_@g9+$5khwfz44z++l(0_(j7d9l9rhnbh'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
